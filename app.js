025d2cab6b8dae93f936aa5168f626e326098ed5
Milos Kovacevic <miloscpt@gmail.com>
2015-10-07 01:25:27 +0200

    GET single item added

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookModel');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-
 var bookRouter = express.Router();
 
-
+//get all items...
 bookRouter.route('/Books')
     .get(function (req, res) {
-        var responseJson = {hello: 'This is my get api'};
-        res.json(responseJson);
+
+        var query = {};
+
+        if(req.query.genre)
+        {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function (err, books) {
+            if(err) res.status(500).send(err);
+            else
+                res.json(books);
+        });
+    });
+//get single item...
+bookRouter.route('/Books/:bookId')
+    .get(function (req, res) {
+
+        Book.findById(req.params.bookId, function (err, book) {
+            if(err) res.status(500).send(err);
+            else
+                res.json(book);
+        });
     });
     
-app.use('/api', bookRouter);
-
-
+app.use('/api', bookRouter); // 'uzmi bookRouter i sve njegove rute nakaci na /api/ tako da kad zahtjevamo /api/Books/ dobijemo neke podatke...'
 
 app.get('/', function (req, res) {
     res.send("Welcome to my API");
 });
+
 
 app.listen(port, function () {
     console.log("Gulp is  running app  on port : " + port);
